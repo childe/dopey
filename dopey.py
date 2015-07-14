2199795ae9ae0f6e9e32bf8eb33321536692be1c
childe <heychilde@gmail.com>
2015-07-14 22:59:34 +0800

    basiclly completed

--- dopey.py
+++ dopey.py
@@ -3,12 +3,17 @@
 import yaml
 import re
 import datetime
+import json
+import time
 import argparse
+from threading import Thread
 import logging
 import logging.handlers
 import logging.config
 import elasticsearch
 import curator
+
+records = []
 
 
 def initlog(level=None):
@@ -16,48 +21,14 @@ def initlog(level=None):
     if level is None:
         level = logging.DEBUG if __debug__ else logging.INFO
 
-    config = {
-        "version": 1,
-        "disable_existing_loggers": True,
-        "formatters": {
-            "simple": {
-                "format": "%(asctime)s %(name)s %(levelname)s %(message)s"
-            },
-            "verbose": {
-                "format": "%(asctime)s %(levelname)s %(module)s:%(lineno)d %(message)s"
-            }
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "level": "DEBUG",
-                "formatter": "verbose",
-                "stream": "ext://sys.stdout"
-            },
-            "file_handler": {
-                "class": "logging.handlers.RotatingFileHandler",
-                "level": "DEBUG",
-                "formatter": "verbose",
-                "filename": "dopey.log",
-                "maxBytes": 10*1000**3,  # 10M
-                "backupCount": 5,
-                "encoding": "utf8"
-            }
-        },
-        'root': {
-            'level': level,
-            'handlers': ['file_handler', 'console']
-        },
-        "loggers": {
-            "myloger": {
-                "level": level,
-                "handlers": [
-                    "console"
-                ],
-            }
-        },
-    }
-    logging.config.dictConfig(config)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler = logging.handlers.RotatingFileHandler(
+              "dopey.log", maxBytes=50000000, backupCount=2)
+    handler.setFormatter(formatter)
+
+    root_logger = logging.getLogger('')
+    root_logger.setLevel(logging.DEBUG)
+    root_logger.addHandler(handler)
 
 
 def filter_indices(all_indices, indices_config):
@@ -86,6 +57,7 @@ def filter_indices(all_indices, indices_config):
                 date = datetime.datetime.strptime(r[0], '%Y.%m')
             else:
                 logging.warn('%s dont endswith date' % indexname)
+                not_involved.add(indexname)
                 continue
         date = date.date()
 
@@ -110,7 +82,49 @@ def filter_indices(all_indices, indices_config):
             not_involved.add(indexname)
 
     indices["reallocate"].difference_update(indices["delete"])
+    for k, v in indices.items():
+        indices[k] = list(v)
+    not_involved = list(not_involved)
     return indices, not_involved
+
+
+def get_relo_index_cnt(esclient):
+    cnt = elasticsearch.client.CatClient(esclient).health(h="relo")
+    return int(cnt)
+
+
+def close_indices(esclient, indices):
+    if not indices:
+        return
+    logging.debug("try to close %s" % ','.join(indices))
+    if curator.close_indices(esclient, indices):
+        logging.info('indices closed: %s' % ','.join(indices))
+
+
+def delete_indices(esclient, indices):
+    if not indices:
+        return
+    logging.debug("try to delete %s" % ','.join(indices))
+    if curator.delete_indices(esclient, indices):
+        logging.info('indices deleted: %s' % ','.join(indices))
+
+
+def optimize_index(esclient, index):
+    if curator.optimize_index(
+            esclient,
+            index,
+            max_num_segments=1,
+            request_timeout=10 *
+            3600):
+        logging.info('%s optimized' % index)
+
+
+def optimize_indices(esclient, indices):
+    if not indices:
+        return
+    logging.debug("try to optimize %s" % ','.join(indices))
+    for index in indices:
+        Thread(target=optimize_index, args=(esclient, index,)).start()
 
 
 def main():
@@ -134,22 +148,31 @@ def main():
     action_indices, not_involved = filter_indices(
         all_indices, config['indices'])
     logging.info(action_indices)
+    records.append('all actions: \n%s' % json.dumps(action_indices, indent=2))
     logging.info(not_involved)
+    records.append(
+        'indices not configured: \n%s' %
+        json.dumps(
+            not_involved,
+            indent=2))
 
-    if action_indices['close']:
-        curator.close_indices(esclient, action_indices['close'])
-
-    if action_indices['delete']:
-        curator.delete(esclient, action_indices['delete'])
+    close_indices(esclient, action_indices['close'])
+    delete_indices(esclient, action_indices['delete'])
 
     curator.api.allocation(
         esclient,
-        action_indices['reallocate'],
+        list(action_indices['reallocate']),
         rule="tag=cores8")
 
+    while True:
+        relo_cnt = get_relo_index_cnt(esclient)
+        logging.info("relocation indices count: %s" % relo_cnt)
+        if relo_cnt == 0:
+            break
+        time.sleep(10*60)
 
-    for index in action_indices['optimize']:
-        curator.optimize_index(esclient, index, 1)
+    optimize_indices(esclient, action_indices['optimize'])
 
 if __name__ == '__main__':
     main()
+    print >> open('a', 'a'), '\n'.join(records)
